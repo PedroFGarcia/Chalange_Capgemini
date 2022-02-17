da872101fea700b8d633b4b2f7240f137a8136b8
PedroFGarcia <pedro_felipe199713@hotmail.com>
2022-02-17 19:25:46 -0300

    questões 1 e 2

---
+++ Questao01/questao01/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace questao01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            String teste = "";
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 0; j <= n - i; j++)
+                {
+                    teste += " ";
+                }
+                for (int k = 0; k < i; k++)
+                {
+                    teste += "*";
+                }
+                teste += "\n";
+            }
+
+            Console.WriteLine(teste);
+        }
+    }
+}
---
+++ Questao02/questao02/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace questao01
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string senha = Console.ReadLine();
+
+            if (senha.Length < 6)
+            {
+                int sugestao = 6 - senha.Length;
+                Console.WriteLine($"Senha muito curta, adicione {sugestao} caracteres!");
+            }
+            else
+            {
+                Console.WriteLine("Senha segura!");
+            }
+        }
+    }
+}
