6aa2825d375ec747ad0270a60522fac996942f86
Pedro Garcia <pedro_felipe199713@hotmail.com>
2022-02-18 13:10:44 -0300

    questao03

---
+++ Questao03/questao03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace questao03
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            subString(Console.ReadLine());
+        }
+
+        static void subString(String word)
+        {
+            Dictionary<string, int> map = new Dictionary<string, int>();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                for (int j = i; j < word.Length; j++)
+                {
+                    char[] letter = word.Substring(i, j + 1 - i).ToCharArray();
+                    Array.Sort(letter);
+                    string aux = new string(letter);
+                    if (map.ContainsKey(aux))
+                        map[aux] = map[aux] + 1;
+                    else
+                        map.Add(aux, 1);
+                }
+            }
+            int anagramPairCount = 0;
+            foreach (string key in map.Keys)
+            {
+                int n = map[key];
+                anagramPairCount += (n * (n - 1)) / 2;
+            }
+            Console.Write(anagramPairCount);
+        }
+    }
+}
